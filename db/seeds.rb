1024599ce3e16d1b826b7f8742860adb54988f6e
Rayan Castro <rayancdc@gmail.com>
2018-08-01 00:29:25 -0300

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Review.delete_all
+Test.delete_all
+User.delete_all
+
+puts "Generating Users and Tests"
+emails = %w(rayancdc@gmail.com riza@testae.com edu@testae.com fe@testae.com admin@testae.com wagon@wagon.com john@gmail.com amelia@yahoo.com.br julia@gmail.com natalia@gmail.com)
+emails.each do |email|
+  new_user = User.create!(email: email, password: "testae")
+  2.times do
+    new_test = new_user.tests.build(description: Faker::HitchhikersGuideToTheGalaxy.quote, website: Faker::Internet.url, review_price_cents: rand(500), issue_price_cents: 2000 + rand(2000), status: false)
+    new_test.save
+  end
+end
+
+puts "You have now #{User.all.size} users and #{Test.all.size} tests."
+puts "Generating reviews..."
+Test.all.each do |test|
+  10.times do
+    user = User.all.sample
+    if user != test.owner
+      new_review = test.reviews.build(user_agent: Faker::Internet.user_agent, issue_description: Faker::HowIMetYourMother.quote, issue_score: rand(5))
+      new_review.user = user
+      new_review.save
+    end
+  end
+end
+puts "You have now a total of #{Review.all.size} reviews."

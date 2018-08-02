0879050063faf32291e027693adf29c2ec0f5d6d
Rayan Castro <rayancdc@gmail.com>
2018-08-02 12:28:41 -0300

    Add integration to carrierwave, imagewick, and create migration to update database

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,16 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick # <- Notice this
+
+  storage :fog                    # <- and this
+
+  process resize_to_fit: [800, 800]
+
+
+  version :standard do
+    resize_to_fill 250, 300
+  end
+
+  version :thumb do
+    resize_to_fill 100, 100
+  end
+end
